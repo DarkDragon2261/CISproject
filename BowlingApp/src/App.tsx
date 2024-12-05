8382d17048cfd5f07cbdbe0477a96c1c33fd26dc
MusaShoeb <mshoeb@umich.edu>
2024-12-04 19:33:48 -0500

    Updated Routes

--- BowlingApp/src/App.tsx
+++ BowlingApp/src/App.tsx
@@ -3,14 +3,23 @@ import Signup from './components/Signup/signup';
 import Login from './components/Login/Login';
 import Profile from './components/Profile/profile';
 import Security from './components/Security/SecQuestions';
-import Display from './components/Display/DisplayWins';
 
 
 function App() {
   
 
   return (
-      <Display></Display>
+    <Router>
+    <div className="App">
+      <Routes> 
+        <Route path="/" element={<Signup />} /> 
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/profile" element={<Profile/>} />
+        <Route path = '/SecQuestions' element = {<Security></Security>} />
+      </Routes>
+    </div>
+  </Router>
   )
 }
 
